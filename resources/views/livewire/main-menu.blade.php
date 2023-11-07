517098320d78b853aa1aae49f37fd5cccaad09c1
reik <zangan@bk.ru>
2023-11-08 01:57:11 +0300

    add menu bootstrap styles

--- resources/views/livewire/main-menu.blade.php
+++ resources/views/livewire/main-menu.blade.php
@@ -1,7 +1,17 @@
-<div>
-    <ul>
-        @foreach ($menuItems as $key => $item)
-            <a class="mr-3" href="{{ $key }}">{{ $item }}</a>
-        @endforeach
-    </ul>
-</div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Aethry</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                @foreach ($menuItems as $key => $item)
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ $key }}">{{ $item }}</a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+</nav>
